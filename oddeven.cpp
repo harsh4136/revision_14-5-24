4f53b03ce28713a59ae91f4c96c0533461e606cb
mannbhalodiya <mannbhalodiya9@gmail.com>
2024-06-14 16:38:46 +0530

    resolved conflicts of oddeven.cpp

--- oddeven.cpp
+++ oddeven.cpp
@@ -8,9 +8,36 @@ int add(int a, int b){
 	z = x+y;
 	return z;
 }
-int main(){
+
+int find(int n);
+
+int sub ( int a, int b) {
+return(a-b);
+}
+int main()
+{
+	int n,ans,subt;
 	int a, b;
 	a = 5;
 	b = 5;
-	cout<<add(a,b);
+	cout<<"enter the value of n : ";
+	cin>>n;
+	ans=find(n);
+	cout<<"The subtraction of two number is "<<sub(3,1)<<endl;
+	cout<<add(a,b)<<endl;
+}
+
+int find(int n)
+{
+	if(n%2==0)
+	{
+		cout<<"number is even";
+	}
+	else
+	{
+		cout<<"number is odd";
+	}
+
+	return 0;
+	
 }
